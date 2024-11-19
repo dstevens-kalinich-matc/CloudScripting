bdea007c22d5733f68e84a62d3e59612f5e8df89
Derek Stevens-Kalinich <dstevens-kalinich@madisoncollege.edu>
2024-11-18 19:20:45 -0600

    Attempting to submit all of week 12 activity, possibly only what was left out

---
+++ week12/main.tf
@@ -0,0 +1,32 @@
+/* Configure the Terraform cloud settings*/
+terraform {
+    cloud {
+        organization = "dstevens-kalinich-scripting"
+        workspaces {
+            name = "Derek-Workspace"
+        }
+    }
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">=5.0"
+        }
+    }
+    required_version = ">= 0.14.9"
+}
+/* Define the AWS provider configuration*/
+provider "aws" {
+    region = "us-east-1"
+}
+/* Reference an existing IAM role in AWS*/
+data "aws_iam_role" "lab_role" {
+    name = "LabRole"
+}
+/* Create an AWS Lambda function*/
+resource "aws_lambda_function" "test_lambda"{
+    filename = "stopEC2.zip"
+    function_name = "DerekFunction"
+    role = data.aws_iam_role.lab_role.arn
+    runtime = "python3.9"
+    handler = "stopEC2.lambda_handler"
+}
---
+++ week12/stopEC2.py
@@ -0,0 +1,39 @@
+import boto3
+import json
+# Define the AWS region where the resources are located
+region = 'us-east-1'
+# Create an EC2 client using Boto3 for interacting with the EC2 service
+ec2 = boto3.client('ec2', region_name=region)
+# Define the Lambda function handler
+def lambda_handler(event, context):
+    # Describe EC2 instances with a filter to list only running instances
+    response = ec2.describe_instances(
+
+        Filters=[
+        {
+            'Name': 'instance-state-name',
+            'Values': [
+            'running',
+            ]
+        },
+    ],
+    )
+
+    listofinstanceids = []
+    # Iterate through the response to extract instance details
+    for reservation in response["Reservations"]:
+        instances = reservation["Instances"]
+        for instance in instances:
+            print(instance["InstanceId"])
+            listofinstanceids.append(instance["InstanceId"])
+
+    stop_response = "Nothing needed to be stopped"
+
+    if len(listofinstanceids) != 0:
+        stop_response = ec2.stop_instances(InstanceIds=listofinstanceids,DryRun=False)
+        print(stop_response)
+        # Return a success response with the API call details
+        return {
+            'statusCode': 200,
+            'body': json.dumps(stop_response)
+        }
